a92f36f6cdb0d94acff48cf576aa26d208aa3057
VishupHell <toptoros06@gmail.com>
2026-05-05 21:21:13 +0300

    first version

---
+++ src/backend/app.py
@@ -0,0 +1,20 @@
+
+# app.py
+
+# импорт модуля Flask, который вы ранее установили с помощью
+# pip install Flask==3.1.3 "connexion[swagger-ui]==2.14.1"
+from flask import render_template
+import connexion
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
+
+@app.route("/") # декоратор функции для "/" (корневого URL веб-приложения)
+def home():
+    return render_template("home.html") # функция, выводящая home.html в качестве шаблона
+
+
+
+if __name__ == "__main__":
+    # основной вызов приложения с указанием хоста и порта
+    app.run(host="0.0.0.0", port=8000)
